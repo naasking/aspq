d1db73b9c1bf22ce0b385ca2062198d3721b1cbe
Sandro Magi <naasking@gmail.com>
2012-10-19 17:38:53 -0400

    -use the control name if available and fall back to id if not; now works great

--- AspQ.js
+++ AspQ.js
@@ -2,22 +2,21 @@ var AspQ = function () {
     var queue = new Array();
     var inner = false;
     var async = true;
-    function pb() { inner = true; __doPostBack(queue[0].id, queue[0].args); inner = false; }
-    function gform(id) {
-        var x = $get(id);
+    function pb() { inner = true; var x = queue[0]; __doPostBack(x.el['name'] ? x.el.name : x.el.id, x.args); inner = false; }
+    function gform(x) {
         if (x['form']) return x.form;
         while (x && x.nodeName != 'FORM') x = x.parentNode;
         return x;
     }
     function init(sender, args) {
         async = true;
-        var id = args.get_postBackElement().id;
-        queue[queue.length - 1] = { id: id, args: gform(id).__EVENTARGUMENT.value };
+        var x = args.get_postBackElement();
+        queue[queue.length - 1] = { el: x, args: gform(x).__EVENTARGUMENT.value };
         if (queue.length > 1) args.set_cancel(true);
     }
     function end(sender, args) {
         queue.shift();
-        if (async) setTimeout(pb, 0);
+        if (async && queue.length > 0) setTimeout(pb, 0);
     }
     return {
         init: function () {
@@ -31,8 +30,12 @@ var AspQ = function () {
             if (inner) return true;
             if (!async) return false;
             async = false;
-            queue.push({ id: '', args: '' });
+            queue.push({ el: '', args: '' });
             return typeof Sys !== 'undefined' || queue.length <= 1;
         }
     };
 } ();
+if (typeof Sys != 'undefined')
+{
+    Sys.Application.notifyScriptLoaded();
+}
